161fe4a0ce01f9c1d8ca490fdb869e3661e524e6
Rodrigo Moreira <rodrigoresendemoreira@gmail.com>
2025-07-29 17:19:30 -0300

    feat: adiciona componente da documentação para teste

---
+++ apps/authentication/src/ui/atoms/Task/Task.stories.tsx
@@ -0,0 +1,50 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Task from './Task';
+
+export const ActionsData = {
+  onArchiveTask: () => console.log(`onArchiveTask`),
+  onPinTask: () => console.log(`onPinTask`),
+};
+
+const meta = {
+  component: Task,
+  title: 'Task',
+  tags: ['autodocs'],
+  //👇 Our exports that end in "Data" are not stories.
+  excludeStories: /.*Data$/,
+  args: {
+    ...ActionsData,
+  },
+} satisfies Meta<typeof Task>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    task: {
+      id: '1',
+      title: 'Test Task',
+      state: 'TASK_INBOX',
+    },
+  },
+};
+
+export const Pinned: Story = {
+  args: {
+    task: {
+      ...Default.args.task,
+      state: 'TASK_PINNED',
+    },
+  },
+};
+
+export const Archived: Story = {
+  args: {
+    task: {
+      ...Default.args.task,
+      state: 'TASK_ARCHIVED',
+    },
+  },
+};
