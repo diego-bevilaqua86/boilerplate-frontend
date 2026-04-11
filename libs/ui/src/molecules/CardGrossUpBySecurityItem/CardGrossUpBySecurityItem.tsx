3c3154173770c329cfb5a9fe6d4c765957305a9d
Beatriz Salles <beatrizsallesss@gmail.com>
2026-04-10 22:08:37 -0300

    refactor: usar o DetailRows para componentizar

--- libs/ui/src/molecules/CardGrossUpBySecurityItem/CardGrossUpBySecurityItem.tsx
+++ libs/ui/src/molecules/CardGrossUpBySecurityItem/CardGrossUpBySecurityItem.tsx
@@ -5,6 +5,7 @@ import { useLingui } from '@lingui/react';
 import { Trans } from '@lingui/react/macro';
 import { ActionIcon, Badge, Box, Divider, Group, Paper, Stack, Text, Tooltip } from '@mantine/core';
 import { InfoIcon } from '@phosphor-icons/react';
+import { DetailRow } from '../../atoms/DetailRow/DetailRow';
 
 type CardGrossUpBySecurityItemProps = {
   item: GrossUpBySecurity;
@@ -24,22 +25,18 @@ export const CardGrossUpBySecurityItem = ({ item }: CardGrossUpBySecurityItemPro
       </Stack>
       <Divider />
       <Stack gap="xs" px="md" py="sm">
-        <Group justify="space-between">
-          <Text size="sm" c="dimmed"><Trans>Rentabilidade</Trans></Text>
+        <DetailRow label={<Trans>Rentabilidade</Trans>}>
           <Text size="sm">{percentFormatter(item.rentability, 2)}</Text>
-        </Group>
+        </DetailRow>
         <Box bg="gray.0" py="xs" style={{ borderRadius: 6 }}>
-          <Group justify="space-between">
-            <Text size="sm" c="dimmed"><Trans>Rentabilidade c/ Gross up</Trans></Text>
+          <DetailRow label={<Trans>Rentabilidade c/ Gross up</Trans>}>
             <Text size="sm" fw={600}>{percentFormatter(item.grossUpReturn, 2)}</Text>
-          </Group>
+          </DetailRow>
         </Box>
-        <Group justify="space-between">
-          <Text size="sm" c="dimmed"><Trans>Equivalente</Trans></Text>
+        <DetailRow label={<Trans>Equivalente</Trans>}>
           <Badge variant="light">{item.equivalent}</Badge>
-        </Group>
-        <Group justify="space-between">
-          <Text size="sm" c="dimmed"><Trans>% IR</Trans></Text>
+        </DetailRow>
+        <DetailRow label={<Trans>% IR</Trans>}>
           <Group gap={4}>
             <Text size="sm">{percentFormatter(item.incomeTax, 1)}</Text>
             <Tooltip label={_(msg`Alíquota utilizada para o prazo de 180 até 3260 dias.`)} position="top">
@@ -48,11 +45,10 @@ export const CardGrossUpBySecurityItem = ({ item }: CardGrossUpBySecurityItemPro
               </ActionIcon>
             </Tooltip>
           </Group>
-        </Group>
-        <Group justify="space-between">
-          <Text size="sm" c="dimmed"><Trans>Instituição financeira</Trans></Text>
+        </DetailRow>
+        <DetailRow label={<Trans>Instituição financeira</Trans>}>
           <Text size="sm">{item.entity}</Text>
-        </Group>
+        </DetailRow>
       </Stack>
     </Paper>
   );
